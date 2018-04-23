845bdd1b9884cf7abde077d8801a3249bf873363
davlis <ddewid@gmail.com>
2018-04-23 14:07:33 +0200

    added models loader to config

--- config/hapi.js
+++ config/hapi.js
@@ -6,6 +6,12 @@ const loader = require('../utils/loader');
 module.exports = () => {
     const server = Hapi.server({ port: 3000, host: 'localhost' });
 
+    const models = [];
+
+    loader('../models').forEach((modelPath) => {
+        models.push(require(path.resolve(__dirname, '../models', modelPath)))
+    });
+
     loader('../routes').forEach((routePath) => {
         require(path.resolve(__dirname, '../routes', routePath))(server);
     });
